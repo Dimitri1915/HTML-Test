0074325e95ab7e23753dce9fdc41b72d56568c39
Dimy <jorgealbertosanchezlujan52@gmail.com>
2024-03-14 10:01:09 -0600

    HTML

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HTML-Test
+- Esta es una prueba de HTML con la inclusion de JavaScript.
+- Clonado en caso de perdida.
---
+++ main.js
@@ -0,0 +1,8 @@
+//document.write("Bienvenidos a la Introducción de JavaScript");
+
+//alert("Bienvenidos los aventurados de Informática Administrativa.");
+
+let edadAlumno;
+edadAlumno = 19;
+
+alert("La edad alumno: "+edadAlumno);
